7f63eb1e23ee34b7847346124307531f047af0d2
Joaquín Diaz <joacko724@gmail.com>
2025-10-18 21:25:24 -0300

    Añadir estilos CSS para el dashboard y crear el archivo HTML correspondiente

--- Js/codeup.js
+++ Js/codeup.js
@@ -1,2 +1,16 @@
-const navPrincipal= document.querySelector('.cabecera')
-console.log(navPrincipal);
+// Hace que el movimiento del HTML sea suave cuando hay un click en el "Empezar a aprender"
+window.addEventListener("load", () => {
+    if (window.location.hash) {
+        const target = document.querySelector(window.location.hash);
+    if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+    }
+    }
+});
+
+// Evento para el botón "Iniciar Sesión"
+const logueate = document.querySelector('#btn-lg');
+logueate.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = "registro.html";
+});
